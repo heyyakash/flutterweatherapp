af7ff00b6e6ff55ca64f31b03b47b6f3416c917b
heyyakash <akashsharma2002@gmail.com>
2022-05-25 21:36:34 +0530

    added expanded temperature widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
 // ignore_for_file: prefer_const_constructors, sized_box_for_whitespace
 
 import 'package:flutter/material.dart';
+import 'package:http/http.dart';
+import 'dart:convert';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 void main() {
   runApp(MaterialApp(home: Home()));
@@ -23,21 +26,26 @@ class _HomeState extends State<Home> {
           width: MediaQuery.of(context).size.width,
           color: Colors.red,
           child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
             children: [
               Padding(
-                padding: EdgeInsets.only(top: .0),
+                padding: EdgeInsets.zero,
                 child: Text('Currently in Bangalore',
                     style: TextStyle(
                         color: Colors.white,
                         fontSize: 20.0,
                         fontWeight: FontWeight.w600)),
               ),
-              Text(
-                '23\u0000',
-                style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 14.0,
-                    fontWeight: FontWeight.w600),
+              Padding(
+                padding: EdgeInsets.only(top: 10.0),
+                child: Text(
+                  '23\u2103',
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 50.0,
+                      fontWeight: FontWeight.w400),
+                ),
               ),
               Padding(
                 padding: EdgeInsets.only(top: 10.0),
@@ -48,7 +56,21 @@ class _HomeState extends State<Home> {
                         fontWeight: FontWeight.w600)),
               ),
             ],
-          ))
+          )),
+      Expanded(
+        child: Padding(
+          padding: EdgeInsets.all(20.0),
+          child: ListView(
+            children: [
+              ListTile(
+                leading: FaIcon(FontAwesomeIcons.temperatureHalf),
+                title: Text('Temperature'),
+                trailing: Text('22\u2103'),
+              )
+            ],
+          ),
+        ),
+      )
     ]));
   }
 }
